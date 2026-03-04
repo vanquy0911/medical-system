4b439a937779c5dfe6527189fa2556403ec91b3f
vanquy <quytan27@gmail.com>
2026-03-04 15:20:09 +0700

    first commit

---
+++ src/test/java/com/medical/medical_system/MedicalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.medical_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
